825e9a45a9cc9033dfa033a3abc345eec28c5512
Poorya Salati <72719359+LampStack@users.noreply.github.com>
2022-01-01 17:24:46 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 using namespace std;
 
 const unsigned int size = 26;
@@ -13,14 +14,18 @@ class Trie {
         struct node *root = new (struct node);
 		Trie();
 		struct node* makeNode(void);
-        void insert(struct node *,string word);
-        bool search(struct node *,string word);
-        bool isEmpty(struct node *);
+        void insert(string word);
+        bool search(string word);
+        string readFile(string path);
+        void insert_to_file(string content, char chr);
+        void toUpperCase(string& str);
+        bool isEmpty();
 };
 
 Trie::Trie(){
     root = Trie::makeNode();
 }
+
 struct node* Trie::makeNode(void){
     struct node *newNode = new (struct node);
     newNode->star = false;
@@ -29,7 +34,7 @@ struct node* Trie::makeNode(void){
     return newNode;
 }
 
-void Trie::insert(struct node *root, string word) {
+void Trie::insert(string word) {
     struct node *temp = root;
     for (int i=0; i<word.length(); i++) {
         int index = word[i] - 'A'; // 'A' ascii code in 65, with this code we have 0 index for 'A' and 26 index for 'Z'
@@ -40,7 +45,7 @@ void Trie::insert(struct node *root, string word) {
     temp->star= true;
 }
 
-bool Trie::search(struct node *root, string word) {
+bool Trie::search(string word) {
     struct node *temp = root;
     for (int i=0; i<word.length(); i++) {
         int index = word[i] - 'A'; // 'A' ascii code in 65, with this code we have 0 index for 'A' and 26 index for 'Z'
@@ -52,57 +57,90 @@ bool Trie::search(struct node *root, string word) {
     return true;
 }
 
-bool Trie::isEmpty(node* root) {
+string Trie::readFile(string path){
+    fstream myFile(path, ios::in );
+    string content;
+    getline(myFile, content, '\0');
+    myFile.close();
+    return content;
+}
+
+void Trie::insert_to_file(string path, char chr){
+    string content = readFile(path);
+    string temp = "";
+    for (int i=0; i<(int)content.size(); i++){
+        if (content[i] != chr){
+            temp += content[i];
+        }else{
+            toUpperCase(temp);
+            insert(temp);
+            temp = "";
+        }
+    }
+}
+
+void Trie::toUpperCase(string& str){
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+}
+
+bool Trie::isEmpty() {
     for (int i = 0; i < size; i++)
     if (root->pointer[i])
         return false;
     return true;
 }
 
-void toUpperCase(string& str){
-    transform(str.begin(), str.end(), str.begin(), ::toupper);
-}
-
-
-
 int main() {
     class Trie tree;
     int option;
-    string word;
+    string word, path;
+    system("cls");
     while (true) {
-    cout << "\nWelcome To Trie Tree Program !\n";
-    cout << "Choose an option :\n";
-    cout << "\t1- insert words.\n";
-    cout << "\t2- search words.\n";
-    cout << "\t3- exit app.\n";
-    cout << "> ";
-    cin >> option;
-    switch(option){
-        case 1 :
-            cout << "\nPlease Give Me A Word To Insert : ";
-            cin >> word;
-            toUpperCase(word);
-            if(tree.search(tree.root, word)) {
-                cout << "\nalready exists !\n";
-            } else {
-                tree.insert(tree.root, word);
-                cout << "\nWord successfuly added.\n";
+        cout << "Choose an option :\n";
+        cout << "\t1- add a word\n";
+        cout << "\t2- search a word\n";
+        cout << "\t3- add words from a file\n";
+        cout << "\t4- exit app\n";
+        cout << "> ";
+        cin >> option;
+        system("cls");
+        switch(option){
+            case 1 :
+                cout << "\n> Please Give Me A Word To Insert : ";
+                cin >> word;
+                tree.toUpperCase(word);
+                if(tree.search(word)) {
+                    system("cls");
+                    cout << "\n***This word already exists in the tree***\n";
+                } else {
+                    tree.insert(word);
+                    system("cls");
+                    cout << "\n***The word was successfully added to the tree***\n";
+                }
+            break;
+            case 2 :
+                cout << "\n> Please Give Me A Word To Insert : ";
+                cin >> word;
+                tree.toUpperCase(word);
+                if(tree.search(word)){
+                    system("cls");
+                    cout << "\n***The word was found in the tree***\n";
+                }else{
+                    system("cls");
+                    cout << "\n***This word does not exist in the tree***\n";
+                }
+            break;
+            case 3 :
+                cout << "\n> Please enter the file name : ";
+                cin >> path;
+                tree.insert_to_file(path, ' ');
+                system("cls");
+                cout << "\n***All words in the file were added to the tree***\n";
+            break;
+            case 4 :
+                exit(0);
+            break;
             }
-        break;
-        
-        case 2 :
-            cout << "\nPlease Give Me A Word To Insert : ";
-            cin >> word;
-            toUpperCase(word);
-            if(tree.search(tree.root, word))
-                cout << "\nYES\n";
-            else
-                cout << "\nNo\n";
-        break;
-        case 3 :
-            exit(0);
-        break;
-    }
     }
    return 0;
 }
